a9816b8c6763d524329193f308d6c959c58941a5
adermaulana <adermaulana15@gmail.com>
2022-12-24 13:34:45 +0800

    baru lagi

--- resources/views/booking/daftar.blade.php
+++ resources/views/booking/daftar.blade.php
@@ -27,6 +27,8 @@
 
     </div>
 </div>
+@elseif(Auth::Check())
+<h1 style="margin-bottom:499px">Anda adalah Admin tidak bisa booking<a class="text-decoration-none" href="/dashboard"> Masuk ke Menu Dashboard</a></h1>
 @else
 <h1 style="margin-bottom:499px">Login Terlebih Dahulu <a href="/login">Disini</a></h1>
 @endauth
